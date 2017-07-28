03b8b3a62f2a09088d7194161da20a2bc6301617
anglinb <laxnski@gmail.com>
2017-07-28 14:26:20 -0700

    Intial commit

---
+++ spec/test-atom-package-2-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TestAtomPackage2View from '../lib/test-atom-package-2-view';
+
+describe('TestAtomPackage2View', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
